f8e789bed948ba6ac5774e4c906ae0dd4e38cee1
vital <vital@VitaliiDesh>
2023-11-13 19:17:48 +0200

    Initial Commit.

---
+++ Lab1/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Lab1PavlosiukKI304","l":"Lab1PavlosiukKI304()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Lab1PavlosiukKI304","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
